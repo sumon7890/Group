896d6bddfcc8acc91e81104ac9854afcd5701415
sumon7890 <sumon.ct.2019@gmail.com>
2020-07-25 07:05:11 +0600

    index_htmel & index_js created

---
+++ 11/index.js
@@ -0,0 +1,146 @@
+
+// var rect = function (width, height) {
+
+
+//     this.width = width
+//     this.height = height
+
+
+//     var position = {
+//         x: 45,
+//         y: 66
+//     }
+//     var printract = function () {
+//         console.log('My weight is ' + this.width);
+//         console.log('My height is ' + this.height);
+//     }.bind(this)
+
+
+//     // this.getPosition = function () {
+//     //     return position
+//     // }
+
+//     this.darw = function () {
+//         console.log('I am sumon Khan');
+//         printract()
+//         console.log('Position x = ' + position.x + ' ' + '   Y= ' + position.y);
+
+//     }
+
+//     Object.defineProperty(this, 'position', {
+//         get: function () {
+//             return position
+//         },
+//         set: function (value) {
+//             position = value
+//         }
+//     })
+
+// }
+
+
+
+// var rect1 = new rect(10, 40)
+// rect1.darw()
+
+// rect1.position = {
+//     x: 254,
+//     y: -950
+// }
+
+
+// console.log(rect1.position)
+function extend(praent, child) {
+    praent.prototype = Object.create(child.prototype)
+    praent.prototype.constructor = praent
+}
+//------------------------------------------------------//
+
+function Square(width, color) {
+    Shape.call(this, color)
+    Circel.call(),
+        Sumon.call(),
+        this.width = width
+
+}
+extend(Square, Shape)
+
+Square.prototype.common = function () {
+    console.log('Hello I am Calling methods')
+
+}
+
+
+var sqr = new Square(5, 'Green, Yellow')
+//----------------------------------------------------//
+function Shape(color, name) {
+    this.color = color
+    this.name = name
+}
+
+extend(Square, Shape)
+
+Shape.prototype.common = function () {
+    console.log('I am Sumon For Your Nose');
+
+
+
+}
+
+extend(Square, Shape)
+
+
+
+Square.prototype.darw = function () {
+    console.log('Draw')
+
+}
+
+
+function Circel(color, readis) {
+    Shape.call(this, color)
+    this.readis = readis
+}
+
+extend(Circel, Shape)
+
+Circel.prototype.common = function () {
+    // Sumon.prototype.common.call(this)
+    console.log('Hello I am Circel in the round');
+}
+
+var c = new Circel('Pink', 942)
+
+
+// --------------------------------------------------------------------//
+
+function Sumon(color, name, classt) {
+    Shape.call(this, color)
+    this.name = name
+    this.classt = classt
+}
+
+extend(Sumon, Shape)
+
+Sumon.prototype.common = function () {
+    // Square.prototype.common.call(this),
+    //     Shape.prototype.common.call(this)
+    console.log('Hello I am sumon please override');
+
+}
+
+var m = new Sumon('Black & White', 'Lotif', 'Two')
+
+//------------------------------------------------------------------------//
+
+
+
+// var sap = new Shape()
+
+var s = new Square(220, 'Browran')
+
+var all = [sqr, c, m]
+
+for (var i of all) {
+    i.common()
+}
--- ODD_Or_Even.js
+++ ODD_Or_Even.js
@@ -1,43 +1,75 @@
-var n = 0
+// var n = 0
 
-for (var i = 0; i < 50; i++) {
-    if (n % 2 == 0) {
-        console.log(i);
+// for (var i = 0; i < 50; i++) {
+//     if (n % 2 == 0) {
+//         console.log(i);
 
-    }
-}
+//     }
+// }
 
 
-var sum = 0;
-for (var i = 0; i <= 20; i++) {
-    if (i % 2 == 0) {
-        console.log(sum + ' + ' + i + ' = ' + (sum + i));
-        sum += i
+// var sum = 0;
+// for (var i = 0; i <= 20; i++) {
+//     if (i % 2 == 0) {
+//         console.log(sum + ' + ' + i + ' = ' + (sum + i));
+//         sum += i
 
-    }
-}
+//     }
+// }
+// var sum = 1;
+// for (var i = 0; i <= 20; i++) {
+//     if (i % 2 == 1) {
+//         console.log(sum + ' * ' + i + ' = ' + (sum * i));
+//         sum *= i
+
+//     }
+// }
+
+// var sum = 0;
+// for (var i = 0; i <= 50; i++) {
+//     if (i % 2 == 1) {
+//         console.log(sum + i);
+
+
+//     }
+// }
+// var sum = 0;
+// for (var i = 0; i <= 50; i++) {
+//     if (i % 2 == 0) {
+//         console.log(sum + i);
+
+
+//     }
+// }
+
+
+// var n = 63
+
+// if (n % 2 == 0) {
+//     console.log('Print ODD number');
+
+// } else {
+//     console.log('Print Even Number');
+
+// }
+
+
 var sum = 1;
-for (var i = 0; i <= 20; i++) {
-    if (i % 2 == 1) {
-        console.log(sum + ' + ' + i + ' = ' + (sum + i));
-        sum += i
+for (var i = 0; i < 10; i++) {
 
-    }
+    console.log(sum + ' * ' + i + ' = ' + (sum * i));
+
+
 }
+var sum = 2;
+for (var i = 0; i < 10; i++) {
 
-var sum = 0;
-for (var i = 0; i <= 50; i++) {
-    if (i % 2 == 1) {
-        console.log(sum + i);
-
-
-    }
+    console.log(sum + ' * ' + i + ' = ' + (sum * i));
 }
-var sum = 0;
-for (var i = 0; i <= 50; i++) {
-    if (i % 2 == 0) {
-        console.log(sum + i);
+var sum = 3;
+for (var i = 0; i < 10; i++) {
+
+    console.log(sum + ' * ' + i + ' = ' + (sum * i));
 
 
-    }
 }
