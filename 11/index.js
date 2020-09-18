8735c2dc713238cce193cf9f3a767ce873a88418
sumon7890 <sumon.ct.2019@gmail.com>
2020-09-19 00:06:02 +0600

    new class created in es6

--- 11/index.js
+++ 11/index.js
@@ -50,97 +50,137 @@
 
 
 // console.log(rect1.position)
-function extend(praent, child) {
-    praent.prototype = Object.create(child.prototype)
-    praent.prototype.constructor = praent
+// function extend(praent, child) {
+//     praent.prototype = Object.create(child.prototype)
+//     praent.prototype.constructor = praent
+// }
+// //------------------------------------------------------//
+
+// function Square(width, color) {
+//     Shape.call(this, color)
+//     Circel.call(),
+//         Sumon.call(),
+//         this.width = width
+
+// }
+// extend(Square, Shape)
+
+// Square.prototype.common = function () {
+//     console.log('Hello I am Calling methods')
+
+// }
+
+
+// var sqr = new Square(5, 'Green, Yellow')
+// //----------------------------------------------------//
+// function Shape(color, name) {
+//     this.color = color
+//     this.name = name
+// }
+
+// extend(Square, Shape)
+
+// Shape.prototype.common = function () {
+//     console.log('I am Sumon For Your Nose');
+
+
+
+// }
+
+// extend(Square, Shape)
+
+
+
+// Square.prototype.darw = function () {
+//     console.log('Draw')
+
+// }
+
+
+// function Circel(color, readis) {
+//     Shape.call(this, color)
+//     this.readis = readis
+// }
+
+// extend(Circel, Shape)
+
+// Circel.prototype.common = function () {
+//     // Sumon.prototype.common.call(this)
+//     console.log('Hello I am Circel in the round');
+// }
+
+// var c = new Circel('Pink', 942)
+
+
+// // --------------------------------------------------------------------//
+
+// function Sumon(color, name, classt) {
+//     Shape.call(this, color)
+//     this.name = name
+//     this.classt = classt
+// }
+
+// extend(Sumon, Shape)
+
+// Sumon.prototype.common = function () {
+//     // Square.prototype.common.call(this),
+//     //     Shape.prototype.common.call(this)
+//     console.log('Hello I am sumon please override');
+
+// }
+
+// var m = new Sumon('Black & White', 'Lotif', 'Two')
+
+// //------------------------------------------------------------------------//
+
+
+
+// // var sap = new Shape()
+
+// var s = new Square(220, 'Browran')
+
+// var all = [sqr, c, m]
+
+// for (var i of all) {
+//     i.common()
+// }
+// ----- Class creatd---
+
+// class Recactngle {
+//     constructor(weight, height) {
+//         this.height = height
+//         this.weight = weight
+//     }
+
+
+//     draw() {
+//         console.log('Drowing.......');
+//     }
+
+// }
+
+// let react1 = new Recactngle(33, 99)
+
+class Parson {
+    constructor(name, email) {
+        this.name = name
+        this.email = email
+    }
+
+    print() {
+        console.log(this.name, this.email);
+    }
+
+
+    static create(str) {
+        let parson = JSON.parse(str)
+        return new Parson(parson.name, parson.email)
+    }
 }
-//------------------------------------------------------//
 
-function Square(width, color) {
-    Shape.call(this, color)
-    Circel.call(),
-        Sumon.call(),
-        this.width = width
+let str = `{"name": "Md Anisur Rahman Sumon", "email": "sumon.ct.2019@gmail.com"}`
 
-}
-extend(Square, Shape)
+let p1 = Parson.create(str)
+p1.print()
 
-Square.prototype.common = function () {
-    console.log('Hello I am Calling methods')
-
-}
-
-
-var sqr = new Square(5, 'Green, Yellow')
-//----------------------------------------------------//
-function Shape(color, name) {
-    this.color = color
-    this.name = name
-}
-
-extend(Square, Shape)
-
-Shape.prototype.common = function () {
-    console.log('I am Sumon For Your Nose');
-
-
-
-}
-
-extend(Square, Shape)
-
-
-
-Square.prototype.darw = function () {
-    console.log('Draw')
-
-}
-
-
-function Circel(color, readis) {
-    Shape.call(this, color)
-    this.readis = readis
-}
-
-extend(Circel, Shape)
-
-Circel.prototype.common = function () {
-    // Sumon.prototype.common.call(this)
-    console.log('Hello I am Circel in the round');
-}
-
-var c = new Circel('Pink', 942)
-
-
-// --------------------------------------------------------------------//
-
-function Sumon(color, name, classt) {
-    Shape.call(this, color)
-    this.name = name
-    this.classt = classt
-}
-
-extend(Sumon, Shape)
-
-Sumon.prototype.common = function () {
-    // Square.prototype.common.call(this),
-    //     Shape.prototype.common.call(this)
-    console.log('Hello I am sumon please override');
-
-}
-
-var m = new Sumon('Black & White', 'Lotif', 'Two')
-
-//------------------------------------------------------------------------//
-
-
-
-// var sap = new Shape()
-
-var s = new Square(220, 'Browran')
-
-var all = [sqr, c, m]
-
-for (var i of all) {
-    i.common()
-}
+console.log(typeof p1);
